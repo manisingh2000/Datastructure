15b638f5dedf9cfc9210dcbe1761094ca30a3648
Mani Singh <54077783+manisingh2000@users.noreply.github.com>
2020-08-27 22:22:40 +0530

    Update DivideTwoNumbers.java

--- DivideTwoNumbers.java
+++ DivideTwoNumbers.java
@@ -15,23 +15,24 @@ import java.util.Scanner;
  */
 public class DivideTwoNumbers {
     static int DivideTwoNumbers(long dividend , long divisor){
-        long n=((dividend < 0) ^ (divisor < 0)) ? -1 : 1; 
+        long n=((dividend < 0) ^ (divisor < 0)) ? -1 : 1; //condition given 
+        //convert both in positive
         dividend = Math.abs(dividend);  
         divisor = Math.abs(divisor);
-        long quotient=0;
+        long quotient=0;//initialize the quotient
         long temp =0;
-        for(int i= (int) dividend;i>=divisor;--i){
-            if(temp+(divisor<<i) <=dividend){
-                temp += divisor<<1;
-                quotient |= 1 << i;
+        for(int i= (int) dividend;i>=divisor;--i){//loop test for the highest
+            if(temp+(divisor<<i) <=dividend){//check the condition
+                temp += divisor<<1;//update temp
+                quotient |= 1 << i;//update quo
             }
         }
-        return  (int) (n*quotient);
+        return  (int) (n*quotient);//return statement
     }
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
         int x =sc.nextInt();
         int y =sc.nextInt();
-        System.out.println(divide(x,y));
+        System.out.println(divide(x,y));//division
     }   
 }
